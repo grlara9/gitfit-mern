7691b7f2b9468c8db7db4d05cef7e2abff9c540a
Guillermo <grlara9@gmail.com>
2022-05-20 21:17:13 -0500

    create user model name email and password fields included , using moongose

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req , res)=>{
-    res.status(200).json({message : 'get goals'})
+    res.status(200).json({message : 'get users'})
 })
 
 module.exports = router
