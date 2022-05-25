c1ba9d331b8716e361bc2bf7326befb4d0e5d1ea
Guillermo <grlara9@gmail.com>
2022-05-25 00:16:54 -0500

    hash password and validation on register user route

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,18 +1,35 @@
+const bcrypt = require('bcryptjs/dist/bcrypt');
 const asyncHandler = require('express-async-handler')
+const User =require('../models/userModel')
  
 //since we are returning promises from mongoose database
 const loginUser = asyncHandler(async(req, res) => {
-    res.json({message: 'login user'})
+    const {name, email, password } = req.body;
+
+    if(!name || !email || !password){
+        res.status(400)
+        throw new Error("All fields must be fill out")
+    }
+
+    const userExits = await User.findOne({ email })
+
+    if(userExits){
+        res.status(400)
+        throw new Error('Email already taken')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(password, salt)
 
 })
 
-const registerUser = async(req, res) => {
+const registerUser = asyncHandler(async(req, res) => {
     res.json({message: 'user register'})
-}
+})
 
-const getUser = async(req, res) => {
+const getUser = asyncHandler( async(req, res) => {
     res.json({message: 'user selected'})
-}
+})
 
 module.exports = {
     loginUser,
