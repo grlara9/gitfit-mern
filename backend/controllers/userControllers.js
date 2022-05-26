e6d67abf8e85a4bfebc7874819802ef777f3922a
Guillermo <grlara9@gmail.com>
2022-05-26 00:04:24 -0500

    login functionality implemented, will generate webtoken when user login

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -48,8 +48,23 @@ const registerUser = asyncHandler(async(req, res) => {
     })
 
 const loginUser = asyncHandler(async(req, res) => {
-    res.json({message: 'user '})
-})
+    const { email, password } = req.body
+
+    // Check for user email
+    const user = await User.findOne({ email })
+  
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+       
+      })
+    } else {
+      res.status(400)
+      throw new Error('email and password don\'t match')
+    }
+  })
 
 const getUser = asyncHandler( async(req, res) => {
     res.json({message: 'user selected'})
