5c1c83afd6c665678047fab9f2859682240e3bbd
Guillermo <grlara9@gmail.com>
2022-05-26 23:42:32 -0500

    create middleware function to protect routes

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken')
+const asyncHandler =require('express-async-handler')
+const User = require('../models/userModel')
+
+const protect = asyncHandler(async(req, res, next) => {
+ var token;
+ 
+})
+
+module.exports = protect
