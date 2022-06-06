48ca8bb4d5f4aa185f05a826a0c89da48aa5d645
Guillermo <grlara9@gmail.com>
2022-06-05 21:29:03 -0500

    all three components are ready with all exercises

--- frontend/src/pages/Exercises.js
+++ frontend/src/pages/Exercises.js
@@ -1,9 +1,13 @@
+import Beginner from "../components/BeginnerPlan"
+import Intermediate from "../components/IntermediatePlan"
+import Advanced from "../components/AdvancedPlan"
+
 const Exercises = () =>{
     return(
         <section className="container">
         <h1>Select your Workout Experience</h1>
         <div className="exercise">
-        <select>
+        <select  value={exercise} onChange={handleChange}>
             <option value="selectExercise" > Select Level of exercise </option>
             <option value="beginner">Beginner</option>
             <option value="intermediate">Intermediate</option>
@@ -12,7 +16,9 @@ const Exercises = () =>{
         </div>
 
         <div className="selectionList">
-        
+            {beginnerContentVisible && <Beginner />}
+            {IntermediateContentVisible && <Intermediate />}
+            {AdvancedContentVisible && <Advanced />}
         </div>
     </section>
     )
