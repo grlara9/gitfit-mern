6a12c5784b086b9776cd7ec62e46487f38edea7e
Guillermo <grlara9@gmail.com>
2022-06-05 22:00:01 -0500

    exercise page ready, user can has 3 options depending on users athletic condition

--- frontend/src/pages/Exercises.js
+++ frontend/src/pages/Exercises.js
@@ -1,8 +1,31 @@
+import { useState, useEffect } from "react"
 import Beginner from "../components/BeginnerPlan"
 import Intermediate from "../components/IntermediatePlan"
 import Advanced from "../components/AdvancedPlan"
+import './Exercise.css'
 
 const Exercises = () =>{
+
+    const [exercise, setExercise] = useState("selectExercise")
+    const [beginnerContentVisible, setBeginnerContentVisible] = useState(false)
+    const [IntermediateContentVisible, setIntermediateContentVisible] = useState(false)
+    const [AdvancedContentVisible, setAdvancedContentVisible] = useState(false)
+
+    useEffect(()=> {
+        exercise === "beginner" 
+        ? setBeginnerContentVisible(true) 
+        :setBeginnerContentVisible(false);
+        exercise === 'intermediate' ? setIntermediateContentVisible(true) : setIntermediateContentVisible(false);
+        exercise === 'advanced' ? setAdvancedContentVisible(true) : setAdvancedContentVisible(false);
+    }, [exercise]);
+   
+
+    const handleChange = event =>{
+        setExercise(event.target.value)
+
+    }
+
+
     return(
         <section className="container">
         <h1>Select your Workout Experience</h1>
