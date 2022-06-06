d567093ab1d3a462bf0a00f7d61838254072eeb2
Guillermo <grlara9@gmail.com>
2022-06-05 21:06:54 -0500

    working on form so user can pick an option will have three components for beginners intermediate and advanced

--- frontend/src/pages/Exercises.js
+++ frontend/src/pages/Exercises.js
@@ -1,6 +1,20 @@
 const Exercises = () =>{
     return(
-        <h1>exercise page</h1>
+        <section className="container">
+        <h1>Select your Workout Experience</h1>
+        <div className="exercise">
+        <select>
+            <option value="selectExercise" > Select Level of exercise </option>
+            <option value="beginner">Beginner</option>
+            <option value="intermediate">Intermediate</option>
+            <option value="advanced">Advanced</option>
+        </select>
+        </div>
+
+        <div className="selectionList">
+        
+        </div>
+    </section>
     )
 }
 
